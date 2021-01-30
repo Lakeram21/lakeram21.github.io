e935bd9168cd7b36e53d27ec2668cb3af9caa1f4
Lakeram Narine <nar19002@byui.edu>
2021-01-30 16:20:54 -0700

    preston page

--- coursePortal/coursePortal/lesson4/js/preston.js
+++ coursePortal/coursePortal/lesson4/js/preston.js
@@ -1,5 +1,19 @@
+function toggleMenu() {
+    document.getElementsByClassName("navigation")[0].classList.toggle("responsive")
+}
+
+
+
+const { func } = require("prop-types");
+
 var string = document.lastModified;
 let year = new Date
 year = year.getFullYear()
 document.getElementById('datemodified').textContent = "Last Updated: " + string
 document.getElementById("currentYear").textContent = year
+
+
+
+
+
+
