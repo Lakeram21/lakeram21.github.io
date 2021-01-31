deab627c6999badb5ecd2290669264a0ef0d4d72
Lakeram Narine <nar19002@byui.edu>
2021-01-30 20:38:16 -0700

    ch

--- coursePortal/coursePortal/course.js
+++ coursePortal/coursePortal/course.js
@@ -1,3 +1,14 @@
+var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday",]
+var month = ["January", "Febuary", "March", "April", "May", "June", "July", "August",
+    "September", "October", "Noveber", "December"]
+
+var year = getFullYear()
+
+var date = new Date()
+var day = date.getDate()
+console.log(days[day])
+
+
 var string = document.lastModified;
 let year = new Date
 year = year.getFullYear()
