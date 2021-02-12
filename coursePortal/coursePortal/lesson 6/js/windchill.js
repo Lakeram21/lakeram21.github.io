d94e8e6980d0e020740b21c656da9f809c5c40e5
Lakeram Narine <nar19002@byui.edu>
2021-02-12 14:39:39 -0700

    chnage tem

--- coursePortal/coursePortal/lesson 6/js/windchill.js
+++ coursePortal/coursePortal/lesson 6/js/windchill.js
@@ -1,7 +1,7 @@
 var windchill = document.getElementById("windchill")
 
 var temp = 50
-var wind = 3 ** 0.6
+var wind = 3 ** 0.16
 
 var value = 35.74 + (0.6215 * temp) - (35.75 * wind) + (0.4275 * temp * wind)
 
