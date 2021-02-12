88bdff81bb098aed8db41630b7f43682b82d69dd
Lakeram Narine <nar19002@byui.edu>
2021-02-12 14:29:28 -0700

    windchill

---
+++ coursePortal/coursePortal/lesson 6/js/windchill.js
@@ -0,0 +1,12 @@
+var windchill = document.getElementById("windchill")
+
+var temp = 50
+var wind = 3 ** 0.6
+
+var value = 35.74 + (0.6215 * temp) - (35.75 * wind) + (0.4275 * temp * wind)
+
+var windValue = value.toFixed(2)
+//console.log(windValue, wind)
+
+
+windchill.innerHTML = windValue + " &#8457"
