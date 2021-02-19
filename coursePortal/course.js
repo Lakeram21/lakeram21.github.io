1f62f31fe981a77faf333b73a44f09b8a0db8be8
Lakeram Narine <nar19002@byui.edu>
2021-02-18 18:43:35 -0700

    updatepreston

--- coursePortal/course.js
+++
@@ -1,5 +0,0 @@
-var string = document.lastModified;
-let year = new Date
-year = year.getFullYear()
-document.getElementById('datemodified').textContent = "Last Updated: " + string
-document.getElementById("currentYear").textContent = year
