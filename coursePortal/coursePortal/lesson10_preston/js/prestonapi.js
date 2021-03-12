6569f0eca959edd435d9620d6ce10ed8e58bd3e8
Lakeram Narine <nar19002@byui.edu>
2021-03-11 22:20:40 -0700

    name url

--- coursePortal/coursePortal/lesson10_preston/js/prestonapi.js
+++ coursePortal/coursePortal/lesson10_preston/js/prestonapi.js
@@ -1,9 +1,9 @@
 const city_id = 5604473;
 const api_key = "a04a937677202ae37502ad9609a62fc3"
-const baseUrl = 'https://api.openweathermap.org/data/2.5/weather?id=5604473&appid=a04a937677202ae37502ad9609a62fc3'
+const baseUrl2 = 'https://api.openweathermap.org/data/2.5/weather?id=5604473&appid=a04a937677202ae37502ad9609a62fc3'
 
 //the fetch method
-fetch(baseUrl)
+fetch(baseUrl2)
     .then(function (response) {
         return response.json();
     })
