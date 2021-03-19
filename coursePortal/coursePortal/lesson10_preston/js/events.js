8c0cb3337f23743f583da6e4584aaef8689f0b32
Lakeram Narine <nar19002@byui.edu>
2021-03-18 19:15:52 -0600

    finish up

---
+++ coursePortal/coursePortal/lesson10_preston/js/events.js
@@ -0,0 +1,32 @@
+const requestURL = "https://byui-cit230.github.io/weather/data/towndata.json"
+
+fetch(requestURL)
+    .then((response) => {// this returns a promise and the 
+        return response.json();
+    })
+    .then((jsonObject) => {
+        ;  // temporary checking for valid response and data parsing
+        const towns = jsonObject['towns']
+        console.log(towns)
+
+        //getting the div that was created in the Html
+        const events = document.querySelector('.events')
+
+        //creating preston div
+        const preston = towns[6].events
+
+
+        preston.forEach(element => {
+            const event = document.createElement("p");
+            event.innerHTML = element;
+            events.append(event)
+
+        });
+
+
+
+
+
+    })
+
+
